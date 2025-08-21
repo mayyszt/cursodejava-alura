4fded1ba0eb7c87bc0a2b543079b03e231511f16
Mayra <mayra.goncalves.santos@escola.pr.gov.br>
2025-08-21 10:10:02 -0300

    Add files via upload

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 16;
+const nome = "Mayra";
+let ano = 2008;
+
+//não fazer 
+//mes = "Janeiro";
